24954fc814a88afb39a2577fd88be0b2fd209672
kusuma112003 <127906816+kusuma112003@users.noreply.github.com>
2023-03-29 09:43:14 +0800

    Add files via upload

---
+++ sesi2/latihan02.php
@@ -0,0 +1,8 @@
+<?php
+    $angka1 = 5;
+    $angka2 = 9;
+    if($angka1 == $angka2){
+        echo "Nilai Variabel angka1 sama dengan variabel angka2";
+    }else{
+        echo "Nilai Variabel angka 1 tidak sama dengan variabel angka2";
+    }
---
+++ sesi2/latihan04.php
@@ -0,0 +1,10 @@
+<?php
+    $nAwal = 10;
+    $nAkhir = 10;
+
+    while($nAwal < $nAkhir){
+        //blok di ulang
+        echo "Perulangan ke-".$nAwal."<br>";
+
+        $nAwal++;
+    }
